349bbafbcb147134e485f8ea6452176e00afb5d1
coderismail22 <122710226+coderismail22@users.noreply.github.com>
2023-06-16 18:50:28 +0600

    mongoDB-basic-setup

--- index.js
+++ index.js
@@ -10,5 +10,17 @@ app.use(express.json());
 
 //
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.VITE_USERNAME}:${process.env.VITE_USERPASS}@cluster0.4in3v8j.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 
 
